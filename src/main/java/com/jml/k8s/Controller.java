202e29cb9c9f20436fd86263c8736add86515dee
JML <jian.min.li1985@gmail.com>
2020-10-31 20:10:28 +0800

    first commit

---
+++ src/main/java/com/jml/k8s/Controller.java
@@ -0,0 +1,14 @@
+package com.jml.k8s;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello JML";
+	}
+	
+}
---
+++ src/main/java/com/jml/k8s/K8sSbApplication.java
@@ -0,0 +1,13 @@
+package com.jml.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sSbApplication.class, args);
+	}
+
+}
